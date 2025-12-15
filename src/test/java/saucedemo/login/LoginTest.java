bdad89fd1885e61f52a17c17c04e570c04e7719b
dafitutama09-su <dafitutama09@gmail.com>
2025-12-15 15:24:54 +0700

    exercise-ci-cd-dafit

--- src/test/java/saucedemo/login/LoginTest.java
+++ src/test/java/saucedemo/login/LoginTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest{
 
+    //Login berhasil
     @Test(groups = {"smoke"})
     public void testLogin() {
         LoginPage loginPage = new LoginPage(DriverManager.getDriver());
